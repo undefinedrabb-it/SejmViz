82263e76dfe55be47ce96852224ba4ca2818f1ac
Zoe Zając <undefinedastrorabbit@gmail.com>
2025-03-26 21:24:59 +0100

    fix eslint

--- libs/shared/ui/eslint.config.mjs
+++ libs/shared/ui/eslint.config.mjs
@@ -7,26 +7,26 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
 export default defineConfig([
-    globalIgnores(["!**/*", "**/vite.config.*.timestamp*", "**/vitest.config.*.timestamp*"]),
-    {
-        extends: compat.extends("plugin:@nx/react", "../../../.eslintrc.json"),
-    },
-    {
-        files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-        rules: {},
-    },
-    {
-        files: ["**/*.ts", "**/*.tsx"],
-        rules: {},
-    },
-    {
-        files: ["**/*.js", "**/*.jsx"],
-        rules: {},
-    },
+  globalIgnores(["!**/*", "**/vite.config.*.timestamp*", "**/vitest.config.*.timestamp*"]),
+  {
+    extends: compat.extends("plugin:@nx/react"),
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {},
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {},
+  },
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    rules: {},
+  },
 ]);
